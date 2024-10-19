31a8c0eaf3a982514eb9908db7220d7c0e1748a6
Panshul <panshulsingh2005@gmail.com>
2024-10-19 15:22:48 +0530

    Layout and navbar added

--- frontend/LIB_FRONTEND/src/App.jsx
+++ frontend/LIB_FRONTEND/src/App.jsx
@@ -1,19 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <div></div>
-        <div></div>
-      </div>
+      <div className="h-[80vh] w-full top-[0px]  rotate-[0.43] bg-[#F2E6DC] rounded-b-[50%] absolute -z-0"></div>
+      <Navbar />
+      <div className="h-[80vh] w-full top-[800.79px]  rotate-[0.43] bg-[#F2E6DC] rounded-t-[50%] absolute -z-0"></div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+//rounded-br-[400px] rounded-bl-[400px]
---
+++ frontend/LIB_FRONTEND/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import logo from "../../assets/logo_gif.gif";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full relative flex justify-between items-center h-12 px-5 py-10">
+      <div className="pic text-2xl  font-mono font-bold h-[250px]">
+        <img src={logo} alt="" className="h-full w-full" />
+      </div>
+      <div className="options">
+        <ul className="flex gap-10">
+          <li>
+            <a href="#" className="text-lg font-serif">
+              Reserve Seat
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-lg font-serif">
+              Subscriptions
+            </a>
+          </li>
+          <li>
+            <a href="#" className="text-lg font-serif">
+              About Us
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div className="flex gap-4">
+        <button className="px-4 py-2 rounded-full bg-yellow-300 hover:bg-blue-100">
+          Search
+        </button>
+        <button className="px-4 py-2 rounded-full bg-yellow-300 hover:bg-blue-100">
+          Sign In
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
